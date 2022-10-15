9d997a6b2145800aa35a9d1a8f6fbc1f9dbaae2e
Minu Gavand <91831080+minugavand@users.noreply.github.com>
2022-10-16 00:22:19 +0530

    Update social_distance_detection.py

--- social_distance_detection.py
+++ social_distance_detection.py
@@ -115,6 +115,32 @@ except:
 # Function for nth fibonacci
 # number - Space Optimisation
 # Taking 1st two fibonacci numbers as 0 and 1
+# Python3 code to demonstrate
+# Characters which Occur in More than K Strings
+# using set() + Counter() + loop + items()
+from collections import Counter
+from itertools import chain
+
+def char_ex(strs, k):
+	temp = (set(sub) for sub in strs)
+	counts = Counter(chain.from_iterable(temp))
+	return {chr for chr, count in counts.items() if count >= k}
+
+# Initializing list
+test_list = ['Gfg', 'ise', 'for', 'Geeks']
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Initializing K
+K = 2
+
+# Characters which Occur in More than K Strings
+# using set() + Counter() + loop + items()
+res = char_ex(test_list, K)
+	
+# printing result
+print ("Filtered Characters are : " + str(res))
 
 def fibonacci(n):
 	a = 0
