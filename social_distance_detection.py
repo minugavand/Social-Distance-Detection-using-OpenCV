835b6883938c2136efb26f241e2845e6e734a46a
Minu Gavand <91831080+minugavand@users.noreply.github.com>
2022-10-16 00:23:06 +0530

    Update social_distance_detection.py

--- social_distance_detection.py
+++ social_distance_detection.py
@@ -35,6 +35,28 @@ except AssertionError as error:
 # Load model
 print("\nLoading model...\n")
 network = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# Python3 code to demonstrate
+# Characters which Occur in More than K Strings
+# using set() + Counter() + dictionary comprehension
+from collections import Counter
+
+# Initializing list
+test_list = ['Gfg', 'ise', 'for', 'Geeks']
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Initializing K
+K = 2
+
+# Characters which Occur in More than K Strings
+# using set() + Counter() + dictionary comprehension
+res = {key for key, val in Counter([ele for sub in
+		test_list for ele in set(sub)]).items()
+		if val >= K}
+	
+# printing result
+print ("Filtered Characters are : " + str(res))
 
 print("\nStreaming video using device...\n")
 try:
