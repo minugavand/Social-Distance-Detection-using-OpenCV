c73ed82a601de978f0dc2f65471d0bc823e91072
RaOne <115895885+TheRaOne007@users.noreply.github.com>
2022-10-16 00:17:37 +0530

    Update social_distance_detection.py

--- social_distance_detection.py
+++ social_distance_detection.py
@@ -112,6 +112,39 @@ except:
 
                 label = "{}: {:.3f}%".format(labels[class_id], confidence * 100)
                 print("{}".format(label))
+# Function for nth fibonacci
+# number - Space Optimisation
+# Taking 1st two fibonacci numbers as 0 and 1
+
+def fibonacci(n):
+	a = 0
+	b = 1
+	
+	# Check is n is less
+	# than 0
+	if n < 0:
+		print("Incorrect input")
+		
+	# Check is n is equal
+	# to 0
+	elif n == 0:
+		return 0
+	
+	# Check if n is equal to 1
+	elif n == 1:
+		return b
+	else:
+		for i in range(1, n):
+			c = a + b
+			a = b
+			b = c
+		return b
+
+# Driver Program
+print(fibonacci(9))
+
+# This code is contributed by Saket Modi
+# Then corrected and improved by Himanshu Kanojiya
 
 
                 coordinates[i] = (startX, startY, endX, endY)
